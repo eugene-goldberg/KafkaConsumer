0b9965c29b02cc0a6b6c930102a3d26db0f1249c
Eugene Goldberg <eugene.goldberg117@gmail.com>
2016-05-05 16:18:04 -0500

    added offset awareness - we can start reading the newest messages, or we can start reading from a particular offset

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -8,6 +8,12 @@ using KafkaNet.Model;
 using Newtonsoft.Json;
 using System.Diagnostics;
 
+using KafkaNet; 
+using KafkaNet.Common; 
+using KafkaNet.Model; 
+using KafkaNet.Protocol; 
+
+
 namespace KafkaConsumer
 {
     class Program
@@ -16,7 +22,23 @@ namespace KafkaConsumer
         {
             var options = new KafkaOptions(new Uri("http://40.86.81.216:9092"), new Uri("http://40.86.81.216:9092"));
             var router = new BrokerRouter(options);
-            var consumer = new Consumer(new ConsumerOptions("topic4", router));
+
+            
+            var consumerOffset = new Consumer(new ConsumerOptions("topic4", router));
+
+            var offsets = consumerOffset.GetTopicOffsetAsync("topic4").Result
+                    .Select(x => new OffsetPosition(x.PartitionId, x.Offsets.Max())).ToArray();
+
+            
+            OffsetPosition position = new OffsetPosition();
+            //in real life, we should store the last used offset in a database, retrieve it for each subsecquent restart of the consumer,
+            //and update upon consumer exit, so the next time consumer starts, it will begin reading where it has left off
+            position.Offset = 207;
+            //using this will force the consumer to start reading from a specific explicitly defined position
+            //var consumer = new Consumer(new ConsumerOptions("topic4", router), position);
+
+            //using offsets will cause the consumer to start reading only newly sent messages
+            var consumer = new Consumer(new ConsumerOptions("topic4", router),offsets);
 
             //Consume returns a blocking IEnumerable (ie: never ending stream)
             foreach (var message in consumer.Consume())
